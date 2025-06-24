f89338b2cf76d022d2c4f39479476a3242958504
alexis.fouquet <alexis.fouquet@bts-malraux.net>
2025-06-24 16:50:40 +0200

    Replace previous files with React Vite project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <>
+      <Header />
+<main style={{ padding: "20px", paddingTop: "100px" }}>
+  <h1>Bienvenue sur Réviz</h1>
+</main>
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+    <img src="/src/assets/reviz.png" alt="Logo Réviz" className="logo-image" />
+      <nav className="nav-links">
+        <a href="#">placeholder</a>
+        <a href="#">placeholder</a>
+        <a href="#">placeholder</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
